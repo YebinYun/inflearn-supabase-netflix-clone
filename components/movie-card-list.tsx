b334400ea97d9d892e01cd6f822f43bd77ab57c5
yebin <yebind3308@gmail.com>
2025-03-17 20:40:31 +0900

    Part 3 - 영화 검색 기능 & 영화 개별 상세페이지 구현

--- components/movie-card-list.tsx
+++ components/movie-card-list.tsx
@@ -1,16 +1,27 @@
 "use client";
 
+import { searchMovies } from "actions/movieActions";
 import MovieCard from "./movie-card";
+import { useQuery } from "@tanstack/react-query";
+import { Spinner } from "@material-tailwind/react";
+import { useRecoilValue } from "recoil";
+import { searchState } from "utils/recoil/atoms";
 
 export default function MovieCardList() {
+  const search = useRecoilValue(searchState);
+
+  const searchMoviesQuery = useQuery({
+    queryKey: ["movie", search],
+    queryFn: () => searchMovies(search),
+  });
+
   return (
     <div className="grid gap-1 md:grid-cols-4 grid-cols-3 w-full h-full">
-      <MovieCard />
-      <MovieCard />
-      <MovieCard />
-      <MovieCard />
-      <MovieCard />
-      <MovieCard />
+      {searchMoviesQuery.isLoading && <Spinner />}
+      {searchMoviesQuery.data &&
+        searchMoviesQuery.data.map((movie) => (
+          <MovieCard key={movie.id} movie={movie} />
+        ))}
     </div>
   );
 }
